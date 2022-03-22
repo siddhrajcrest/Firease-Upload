a78979baa74e4c9abb852fcfdafe9f574387a802
siddhrajcrest <s.gohil@crestinfosystems.net>
2022-03-22 10:59:07 +0530

    Created react app

---
+++ src/App.js
@@ -0,0 +1,124 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  updateDoc,
+} from "firebase/firestore";
+import { db, storage } from "./firebase-config";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+
+function App() {
+  const [users, setusers] = useState([]);
+  const [data, setdata] = useState({});
+  const [files, setfiles] = useState();
+  const userCollection = collection(db, "users");
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(userCollection);
+      setusers(
+        data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }))
+      );
+      console.log(users);
+    };
+    getUsers();
+    return () => {};
+  }, []);
+
+  const addUser = async () => {
+    await addDoc(userCollection, data);
+  };
+  const updateUser = async (id, age) => {
+    const userDoc = doc(db, "users", id);
+    const newFields = { age: age + 1 };
+    await updateDoc(userDoc, newFields);
+  };
+  const deleteUser = async (id) => {
+    const userDoc = doc(db, "users", id);
+    await deleteDoc(userDoc);
+  };
+  // const handleUpload = () => {
+  //   const fileName = new Date().getTime() + files.name;
+  //   const storageRef = ref(storage, `/images/${fileName}`);
+  //   const uploadTask = uploadBytesResumable(storageRef, files);
+  //   uploadTask.on(
+  //     (snapshot) => {
+  //       const uploaded =
+  //         Math.floor(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+  //     },
+  //     (error) => {
+  //       console.log(error);
+  //     },
+  //     () => {
+  //       // getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+  //       //   console.log(url);
+  //       // });
+  //       uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+  //         console.log("File available at", downloadURL);
+  //       });
+  //     }
+  //   );
+  // };
+
+  const handleUpload = () => {
+    const fileName = new Date().getTime() + files.name;
+    const storageRef = ref(storage, `/images/${fileName}`);
+    const uploadTask = uploadBytesResumable(storageRef, files);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const uploaded = Math.floor(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+      },
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+          console.log(url);
+        });
+      }
+    );
+  };
+  return (
+    <div className="App">
+      <input
+        placeholder="Name..."
+        onChange={(e) => setdata({ ...data, name: e.target.value })}
+      />
+      <input
+        type="number"
+        placeholder="Age..."
+        onChange={(e) => setdata({ ...data, age: e.target.value })}
+      />
+
+      <button onClick={addUser}>Create user</button>
+      {users.map((user, i) => {
+        return (
+          <>
+            <p>Name: {user.name}</p>
+            <p>Age: {user.age}</p>{" "}
+            <button onClick={() => updateUser(user.id, user.age)}>
+              increase age
+            </button>
+            <button onClick={() => deleteUser(user.id)}>Delete User</button>
+          </>
+        );
+      })}
+      <input
+        type="file"
+        onChange={(e) => setfiles(e.currentTarget.files[0])}
+      ></input>
+      <button onClick={() => handleUpload()}>Upload</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase-config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+import { getStorage } from "firebase/storage";
+const firebaseConfig = {
+  apiKey: "AIzaSyCcFO084ivgjty_CPMuTurgFk3bRqtBORA",
+  authDomain: "tododemo-f5966.firebaseapp.com",
+  databaseURL: "https://tododemo-f5966-default-rtdb.firebaseio.com",
+  projectId: "tododemo-f5966",
+  storageBucket: "tododemo-f5966.appspot.com",
+  messagingSenderId: "890790857981",
+  appId: "1:890790857981:web:720728043a366f68a50294",
+  measurementId: "G-CM9ZTSJKPV",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const storage = getStorage(app, "gs://tododemo-f5966.appspot.com");
